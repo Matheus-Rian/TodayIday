ae47f7a9618ad9c2d6c6abd93b7886262dc8f3ae
Matheus Rian <souzamatheusrian@gmail.com>
2021-01-26 23:19:08 -0200

    35° Dia

---
+++ javaScript/boasPraticas.js
@@ -0,0 +1,13 @@
+// ? Boas Práticas
+
+// TODO 1 - Nomes de variáveis boolean
+
+// ! Forma Errada
+const animal = true;
+const claws = true;
+const fly = true;
+
+// * Forma correta
+const isAnimal = true;
+const hasClaws = true;
+const canFly = true;
