080fe524dd43804b78bbce49bc76a26242f5a81b
Matheus Rian <souzamatheusrian@gmail.com>
2021-01-30 23:29:10 -0200

    36° Dia

---
+++ javaScript/stringMethods.js
@@ -0,0 +1,7 @@
+
+//charAt - Returns the character at the specified index.
+"Hello World".charAt(2); // returns "l"
+// If we pass no value it defaults to an index of 0
+"Hello World".charAt(); // returns "H"
+// If we add an index that is undefined we get an empty string
+"Hello World".charAt(20); // returns ""
